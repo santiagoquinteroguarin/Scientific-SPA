3b836ba754fb031a82559031706d34e9b61eea22
Santiago <quinterosantiago15@gmail.com>
2020-10-29 10:21:34 -0500

    modify route

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
         <div class="Header-main">
             <div class="Header-logo">
                 <h1>
-                    <a href="/">The Rick and Morty</a>
+                    <a href="/Scientific-SPA">The Rick and Morty</a>
                 </h1>
             </div>
             <div class="Header-nav">
